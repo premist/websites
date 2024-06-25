3b8ed57578a3239aab60e7f403fe207b2bb17ece
Minku Lee <public@premi.st>
2024-06-25 13:59:39 +0900

    minor style adjustments

--- apps/premi.st/src/app/page.tsx
+++ apps/premi.st/src/app/page.tsx
@@ -8,16 +8,16 @@ export default function Home(): JSX.Element {
         title="minku lee"
         tagline="home"></Header>
 
-      <div className="container px-10 md:px-20 py-20 md:py-40 flex lg:flex-row flex-col grow">
+      <div className="container px-8 md:px-20 py-16 md:py-40 flex lg:flex-row flex-col grow">
         <h2 className="lg:w-[50%] py-1 pr-10 text-4xl leading-[3rem]">Hello from Korea.</h2>
         <span className="lg:w-[50%] py-1 text-4xl leading-[3rem]">Craft, design, explore,<br />somewhere in between.</span>
       </div>
 
       <div>
-        <div className="border-t-2 border-neutral-200 ml-12 md:ml-20" />
+        <div className="border-t-2 border-neutral-200 ml-8 md:ml-20" />
 
-        <div className="container px-10 md:px-20 py-5 md:py-10 flex flex-col md:flex-row">
-          <p className="md:w-[50%] pr-10">
+        <div className="container px-8 md:px-20 py-5 md:py-10 flex flex-col md:flex-row">
+          <p className="md:w-[50%] pr-8">
             <Link href="https://si.mpli.st" target="_blank">blog &rarr; si.mpli.st</Link>
           </p>
           <p className="md:w-[50%]">contact &rarr; public@premi.st</p>
--- packages/ui/src/header.tsx
+++ packages/ui/src/header.tsx
@@ -8,7 +8,7 @@ export function Header({
   tagline: string;
 }): JSX.Element {
   return (
-    <div className="container px-10 md:px-20 py-5 md:py-10 flex flex-col md:flex-row">
+    <div className="container px-8 md:px-20 py-5 md:py-10 flex flex-col md:flex-row">
       <h1 className="md:w-[20%] pr-10">{title}</h1>
       <span className="md:w-[30%] pr-10">{subtitle}</span>
       <p className="md:w-[50%]">{tagline}</p>
